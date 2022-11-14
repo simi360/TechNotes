ab8f5569740f89825e0664d76f4cdf26a3c97ca0
Harsimran Kaur <infamously13@gmail.com>
2022-11-13 20:44:12 -0500

    Removed websites from allowed origins for CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,9 +2,7 @@ const allowedOrigins = [
     'http://localhost:3500/',
     //random URLS 
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com',
-    'https://www.google.com',
-    'https://google.com'
+    'https://dandrepairshop.com'
 ]
 
 module.exports = allowedOrigins
