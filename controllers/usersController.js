3725b693f323e1dfeef514824092cd6a7034415e
Harsimran Kaur <infamously13@gmail.com>
2022-11-17 20:50:45 -0500

    updating user controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,14 +7,32 @@ const bcrypt = require('bcrypt')
 //@route GET /users
 //@access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-
+    const users = await User.find().select('-password').lean()
+    if (!users) {
+        return res.status(400).json({message: 'No user found'})
+    }
+    res.json(users)
 })
 
 //@desc Create new user
 //@route POST /users
 //@access Private
 const createNewUser = asyncHandler(async (req, res) => {
+    const {username, password, roles } = req.body
 
+    //confirm
+    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    //Check for duplicates
+    const duplicate = await User.findOne({username}).lean().exec()
+    if(duplicate) {
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    //Hash password
+    const hashedPwd = await bcrypt.hash(password, 10) //salt round
 })
 
 //@desc Update a user
